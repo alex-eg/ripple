bcfeaa4a022357a54c8465e10b223a06a4f81d86
taptap <alexey.e.egorov@gmail.com>
2013-06-10 15:23:15 +0400

    changed array creation

--- vector.lisp
+++ vector.lisp
@@ -1,12 +1,12 @@
 (in-package :v)
 
-(defun x (vec)
+(defun x (v)
   (aref v 0))
 
-(defun y (vec)
+(defun y (v)
   (aref v 1))
 
-(defun z (vec)
+(defun z (v)
   (aref v 2))
 
 (defun mag (vec)
@@ -21,22 +21,22 @@
   "Returns normalized vector"
   (let ((len (mag vec)))
     (if (= len 0.0)
-	(make-array len :initial-element 0.0)
+	(make-array (length vec) :initial-element 0.0)
 	(map 'vector 
 	     (lambda (x)
 	       (/ x len))
 	     vec))))
 		   	
 (defun cross (a b)
-  "Returns cross product of two vectors"
-  (assert (= (length a) (length b) 3))
-  (let ((a-x (v:x a))
-	(a-y (v:y a))
-	(a-z (v:z a))
+  (let ((a-x (x a))
+	(a-y (y a))
+	(a-z (z a))
 	
-	(b-x (v:x b))
-	(b-y (v:y b))
-	(b-z (v:z b)))
-    #2a((- (* a-y b-z) (* b-y a-z))
-	(- (* a-z b-z) (* b-z a-x))
-	(- (* a-x b-y) (* b-x a-y)))))
+	(b-x (x b))
+	(b-y (y b))
+	(b-z (z b)))
+    (make-array 3 
+		:initial-contents
+		(list (- (* a-y b-z) (* b-y a-z))
+		      (- (* a-z b-z) (* b-z a-x))
+		      (- (* a-x b-y) (* b-x a-y))))))
