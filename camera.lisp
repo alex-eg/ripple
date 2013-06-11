cb3d563d8f719ce1127e824ef3c4d6305db22357
taptap <alexey.e.egorov@gmail.com>
2013-06-11 23:40:00 +0400

    changed slot names and accessors

--- camera.lisp
+++ camera.lisp
@@ -4,15 +4,15 @@
   ((position 
     :accessor cam-position
     :initarg :position
-    :initform #(0 -2 2))
+    :initform #(3 -3 3))
    (upvec 
-    :accessor cam-upvec
-    :initarg :upvec
+    :accessor cam-up
+    :initarg :up
     :initform #(0 -1 0))
    (sight 
     :accessor cam-sight 
     :initarg :sight
-    :initform #(0 0 0))
+    :initform #(2 0 2))
    (field-of-view-y 
     :accessor cam-fovy
     :initarg :fov
@@ -20,7 +20,7 @@
    (aspect-ratio
     :accessor cam-aspect
     :initarg :aspect
-    :initform 1.0)
+    :initform 4/3)
    (z-near-clip
     :accessor cam-znear
     :initarg :z-near
@@ -41,7 +41,7 @@
   (gl:load-identity)
   (let ((eye (cam-position cam))
 	(center (cam-sight cam))
-	(up (cam-upvec cam)))
+	(up (cam-up cam)))
     (glu:look-at (v:x eye) (v:y eye) (v:z eye)
 		 (v:x center) (v:y center) (v:z center)
 		 (v:x up) (v:y up) (v:z up))))
