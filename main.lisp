b815abed59e1f11c9fe7a89b898b7b70c6f0c429
alex_e <alexey.e.egorov@gmail.com>
2013-07-04 13:13:27 +0400

    fixed mixed up rotation axises

--- main.lisp
+++ main.lisp
@@ -77,8 +77,8 @@
       (:mouse-motion-event 
        (:x-rel dx :y-rel dy)
        (when (sdl:mouse-right-p) ;; rotate view
-	 (camera:rotate-yaw *cam* (/ dy 10.0))
-	 (camera:rotate-pitch *cam* (/ dx 10.0)))
+	 (camera:rotate-yaw *cam* (/ dx 10.0))
+	 (camera:rotate-pitch *cam* (/ dy 10.0)))
        (when (sdl:mouse-left-p) ;; move through the field
 	 (camera:with-old-parameters (*cam* :eye eye
 					    :center center
