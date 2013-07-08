24302851898d42474a5e891fc244a0b7568c8b35
alex_e <alexey.e.egorov@gmail.com>
2013-07-08 14:25:15 +0400

    added function to determine image compression

--- texture.lisp
+++ texture.lisp
@@ -9,9 +9,15 @@
     :reader tex-type)
    (height :accessor tex-height)
    (width :accessor tex-width)))
+
+(defstruct targa-texture
+  data
+  height
+  width
+  type)
  
 (defmethod load-from-file ((tex texture) (file pathname))
-  (let ((file (load-targa-file pathname)))
+  (let ((targa-texture (load-targa-file pathname)))
     ))
     
 (defun load-targa-file (path)
@@ -23,13 +29,27 @@
 	       (read-sequence spec stream)
 	       (file-position stream oldpos)
 	       (string= "TRUEVISION-XFILE"
-			(map 'string #'code-char spec)))))
+			(map 'string #'code-char spec))))
+	     (compressed? (stream)
+	       (let ((header (make-array 12
+					 :element-type '(unsigned-byte 8)))
+		     (oldpos (file-position stream)))
+		 (file-position stream 0)
+		 (read-sequence header stream)
+		 (file-position stream oldpos)
+		 (equal (aref header 2) 10))))
     (with-open-file (tga path
 			 :direction :input
 			 :if-does-not-exist :error
 			 :element-type '(unsigned-byte 8))
-      (let ((len (file-length tga)))
+      (let ((len (file-length tga))
+	    (tex-struct (make-targa-texture)))
 	(or (valid-targa? tga len)
 	    (error "File ~A is not of a valid New Targa Format" path))
+	(format t "This file contains ~Ax~A image, color encoding is ~A~%"
+		(
+	(if (compressed? tga)
+	    
+	    (format t "File is not a compressed targa"))))))
 	
 
