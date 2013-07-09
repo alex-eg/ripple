ad5415fded06ecdf557c4916d400cec516095ae6
alex_e <alexey.e.egorov@gmail.com>
2013-07-09 16:05:26 +0400

    slightly changed shader structure

--- shader.lisp
+++ shader.lisp
@@ -1,10 +1,25 @@
 (in-package :shader)
 
 (defclass shader (resource)
-  )
+  ((filename 
+    :accessor shader-filename
+    :initarg :path)
+   (compiled?
+    :accessor shader-program-compiled?
+    :initform nil)
+   (type
+    :accessor shader-type
+    :initarg :shader-type)
+   (compiled?
+    :accessor shader-compiled?
+    :iniform nil)
+   (source
+    :accessor shader-source)))
+   
+(defclass shader-program (resource)
+  ((linked?
+    :accessor shader-program-linked?
+    :initform nil)))
 
-(defclass vertex-shader (shader)
-  )
-
-(defclass fragment-shader (shader)
-  )
+    
+    
