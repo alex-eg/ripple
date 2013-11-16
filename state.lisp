a5d87b7799576b72a8e235594a28d22b2815a914
taptap <alexey.e.egorov@gmail.com>
2013-11-16 17:38:57 +0400

    add various resource pools; shader set functions

--- state.lisp
+++ state.lisp
@@ -3,6 +3,20 @@
 (defclass state ()
   ((vao
     :accessor state-vao
-    :initform nil)))
+    :initform nil)
+   (shader-pool
+    :accessor shader-pool
+    :initform (make-hash-table))
+   (camera-pool
+    :accessor camera-pool
+    :initform (make-hash-table))
+   (texture-pool
+    :accessor texture-pool
+    :initforn (make-hash-table))))
 
+(defun use-shader-program (state program-name)
+  (let ((shader (gethash program-name (shader-pool state))))
+    (gl:use-program (shader:program-id shader))))
 
+(defun use-default-program ()
+  (gl:use-program 0))
