d5f8c7ac4ccc8db814a47d35d14a2dd6016df0e0
taptap <alexey.e.egorov@gmail.com>
2013-11-17 15:07:23 +0400

    use state object instead of separate variables

--- main.lisp
+++ main.lisp
@@ -58,11 +58,18 @@
     (gl:tex-coord 0.0 4.0)
     (gl:vertex 10.0 5.0 -5.0)))
 
-(let ((cam (make-instance 'camera:camera
-                          :center #(1.0 0.0 0.0)
-                          :eye #(0.0 0.0 0.0)))
-      (texture nil)
-      (cur-state (make-instance 'state:state)))
+(let ((current-state (make-instance
+                      'state:state)))
+  (state:add current-state 'camera "main" 
+             (make-instance 'camera:camera
+                            :center #(1.0 0.0 0.0)
+                            :eye #(0.0 0.0 0.0)))
+  (state:add current-state 'texture "checker"
+             (texture:load-from-file
+              (make-instance 'texture:texture)
+              #P"./resources/textures/checker.tga"))
+  (state:add current-state 'shader "blinn"
+             (make-instance 'shader:shader-program))
   (defun main-loop ()
     (sdl:with-init ()
       (sdl:window 800 600
@@ -79,14 +86,13 @@
       (cl-glut:init)
       (gl:enable :texture-2d)
       (gl:enable :depth-test)
-      (let ((tex (texture:load-from-file
-                  (make-instance 'texture:texture)
-                  #P"./resources/textures/checker.tga"))
-            (blinn (make-instance 'shader:shader-program)))
+      (let ((blinn (state:get current-state 'shader "blinn"))
+            (tex (state:get current-state 'texture "checker"))
+            (cam (state:get current-state 'camera "main"))
+            (texture (first (gl:gen-textures 1))))
         (shader:set-shader blinn #P"./resources/shaders/light.frag.glsl" :fragment-shader)
         (shader:set-shader blinn #P"./resources/shaders/light.vert.glsl" :vertex-shader)
         (shader:compile-program blinn)
-        (setf texture (first (gl:gen-textures 1)))
         (gl:bind-texture :texture-2d texture)
         (gl:tex-parameter :texture-2d :texture-wrap-s :repeat)
         (gl:tex-parameter :texture-2d :texture-wrap-r :repeat)
@@ -99,68 +105,70 @@
                          0 
                          (texture:tex-type tex)
                          :unsigned-byte
-                         (texture:tex-data tex)))
+                         (texture:tex-data tex))
 
       ;;Creating VAO
-      (setf (state:state-vao cur-state) (gl:gen-vertex-array))
-      (gl:bind-vertex-array (state:state-vao cur-state))
+        (setf (state:state-vao current-state) (gl:gen-vertex-array))
+        (gl:bind-vertex-array (state:state-vao current-state))
 
-      (sdl:with-events ()
-        (:key-down-event
-         (:key key)
-         (when (sdl:key= key :sdl-key-q)
-           (camera:rotate-roll cam 1.9))
-         (when (sdl:key= key :sdl-key-e)
-           (camera:rotate-roll cam -1.9)))
+        (sdl:with-events ()
+          (:key-down-event
+           (:key key)
+           (when (sdl:key= key :sdl-key-q)
+             (camera:rotate-roll cam 1.9))
+           (when (sdl:key= key :sdl-key-e)
+             (camera:rotate-roll cam -1.9)))
 
-        (:quit-event () t)
-        (:mouse-button-down-event
-         (:button b)
-         (when (sdl:key= b sdl:sdl-button-wheel-up)
-           (camera:with-old-parameters (cam :eye old-eye
-                                            :center old-center
-                                            :view old-view)
-             (let ((new-pos (v:+ old-view old-eye))
-                   (new-center (v:+ old-center old-view)))
-               (setf (camera:cam-eye cam) new-pos)
-               (setf (camera:cam-center cam) new-center))))
+          (:quit-event () t)
+          (:mouse-button-down-event
+           (:button b)
+           (when (sdl:key= b sdl:sdl-button-wheel-up)
+             (camera:with-old-parameters (cam :eye old-eye
+                                              :center old-center
+                                              :view old-view)
+               (let ((new-pos (v:+ old-view old-eye))
+                     (new-center (v:+ old-center old-view)))
+                 (setf (camera:cam-eye cam) new-pos)
+                 (setf (camera:cam-center cam) new-center))))
            
 
-         (when (sdl:key= b sdl:sdl-button-wheel-down)
-           (camera:with-old-parameters (cam :eye old-eye
-                                            :center old-center
-                                            :view old-view)
-             (let ((new-pos (v:- old-eye old-view))
-                   (new-center (v:- old-center old-view)))
-               (setf (camera:cam-center cam) new-center)
-               (setf (camera:cam-eye cam) new-pos)))))
+           (when (sdl:key= b sdl:sdl-button-wheel-down)
+             (camera:with-old-parameters (cam :eye old-eye
+                                              :center old-center
+                                              :view old-view)
+               (let ((new-pos (v:- old-eye old-view))
+                     (new-center (v:- old-center old-view)))
+                 (setf (camera:cam-center cam) new-center)
+                 (setf (camera:cam-eye cam) new-pos)))))
 
-        (:mouse-motion-event
-         (:x-rel dx :y-rel dy)
-         (when (sdl:mouse-right-p) ;; rotate view
-           (camera:rotate-yaw cam (/ dx -10.0))
-           (camera:rotate-pitch cam (/ dy 10.0)))
-         (when (sdl:mouse-left-p) ;; move through the field
-           (camera:with-old-parameters (cam :eye eye
-                                            :center center
-                                            :up up
-                                            :view view)
-             (let* ((old-dir (vector (v:x view) 0.0 (v:z view)))
-                    (strafe (v:cross up view))
-                    (d-dir (v:+ (v:*. old-dir (/ dy 10))
+          (:mouse-motion-event
+           (:x-rel dx :y-rel dy)
+           (when (sdl:mouse-right-p) ;; rotate view
+             (camera:rotate-yaw cam (/ dx -10.0))
+             (camera:rotate-pitch cam (/ dy 10.0)))
+           (when (sdl:mouse-left-p) ;; move through the field
+             (camera:with-old-parameters (cam :eye eye
+                                              :center center
+                                              :up up
+                                              :view view)
+               (let* ((old-dir (vector (v:x view) 0.0 (v:z view)))
+                      (strafe (v:cross up view))
+                      (d-dir (v:+ (v:*. old-dir (/ dy 10))
                                   (v:*. strafe (/ dx 10))))
 
-                    (new-eye (v:+ eye d-dir))
-                    (new-center (v:+ center d-dir)))
+                      (new-eye (v:+ eye d-dir))
+                      (new-center (v:+ center d-dir)))
 
-               (setf (camera:cam-eye cam) new-eye)
-               (setf (camera:cam-center cam) new-center)))))
-        (:idle ()
-               ;; this lets slime keep working while the main loop is running
-               ;; in sbcl using the :fd-handler swank:*communication-style*
-               ;; (something similar might help in some other lisps, not sure which though)
-               #+(and sbcl (not sb-thread)) (restartable
-                                              (sb-sys:serve-all-events 0))
-               (restartable (draw cam texture)))))))
-;;-------------------------
+                 (setf (camera:cam-eye cam) new-eye)
+                 (setf (camera:cam-center cam) new-center)))))
+          (:idle ()
+                 ;; this lets slime keep working while the main loop is running
+                 ;; in sbcl using the :fd-handler swank:*communication-style*
+                 ;; (something similar might help in some other lisps, not sure which though)
+                 #+(and sbcl (not sb-thread)) (restartable
+                                                (sb-sys:serve-all-events 0))
+                 (restartable (draw 
+                               cam
+                               texture))))))))
+;;-----------------------------------------------
 (main-loop)
