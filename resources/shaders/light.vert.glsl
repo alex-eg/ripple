29cb14d28527c0705b492175c723e0b945cf3dde
taptap <alexey.e.egorov@gmail.com>
2013-11-18 18:29:44 +0400

    moved uniforms

--- resources/shaders/light.vert.glsl
+++ resources/shaders/light.vert.glsl
@@ -3,12 +3,12 @@
 layout(location = 0) in vec4 vertexPosition;
 layout(location = 1) in vec3 vertexNormal;
 
+uniform mat4 MV;
+uniform mat4 P;
+
 out vec3 myNormal;
 out vec4 myVertex;
 out mat4 MVf;
-
-uniform mat4 MV;
-uniform mat4 P;
 
 void main() {
     gl_Position = P * MV * vertexPosition;
