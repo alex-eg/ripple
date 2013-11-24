216c42fc0fbfd632343efe55ff3c7463827941bc
taptap <alexey.e.egorov@gmail.com>
2013-11-24 22:52:53 +0400

    rewrite load-mesh function using gl arrays

--- mesh.lisp
+++ mesh.lisp
@@ -11,14 +11,26 @@
     :initform 0
     :accessor verts-count)))
 
-(defun load-mesh (mesh vertex-vector normal-vector)
-  (setf (vertex-buffer-id mesh) (gl:gen-buffers 1))
+(defun load-mesh (mesh vertex-list normal-list)
+  (setf (vertex-buffer-id mesh) (car (gl:gen-buffers 1)))
+  (setf (verts-count mesh) (length vertex-list))
   (gl:bind-buffer :array-buffer (vertex-buffer-id mesh))
-  (gl:buffer-data :array-buffer :static-draw vertex-vector)
+  (let* ((elt-len (length (car vertex-list)))
+         (arr (gl:alloc-gl-array :float (* (length vertex-list)
+                                           elt-len))))
+    (dotimes (i (length vertex-list))
+      (dotimes (j elt-len)
+        (setf (gl:glaref arr (+ (* i elt-len) j))
+              (svref (elt vertex-list i) j))))
+    (gl:buffer-data :array-buffer :static-draw arr))
 
-  (setf (normal-buffer-id mesh) (gl:gen-buffers 1))
+  (setf (normal-buffer-id mesh) (car (gl:gen-buffers 1)))
   (gl:bind-buffer :array-buffer (normal-buffer-id mesh))
-  (gl:buffer-data :array-buffer :static-draw normal-vector))
-        
-
-  
+  (let* ((elt-len (length (car normal-list)))
+         (arr (gl:alloc-gl-array :float (* (length normal-list)
+                                           elt-len))))
+    (dotimes (i (length normal-list))
+      (dotimes (j elt-len)
+        (setf (gl:glaref arr (+ (* i elt-len) j))
+              (svref (elt normal-list i) j))))
+    (gl:buffer-data :array-buffer :static-draw arr)))
