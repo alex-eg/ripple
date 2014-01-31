1fd00cf960da36c1e7a1d107648ac7c2e4f063ff
taptap <alexey.e.egorov@gmail.com>
2014-01-31 21:29:49 +0400

    remove unused function

--- helpers.lisp
+++ helpers.lisp
@@ -9,17 +9,3 @@
 
 (defmacro make-regular-symbol (symbol-name package-name)
   `(values (intern (string-upcase ,symbol-name) ,package-name)))
-
-(defmacro delimited-read (raw-stream delimiter)
-  (let ((char (gensym))
-        (token (gensym))
-        (stream (or (and (atom raw-stream) raw-stream)
-                    (eval raw-stream))))
-    `(with-output-to-string (,token)
-       (do ((,char (read-char ,stream nil :eof)
-                   (read-char ,stream nil :eof)))
-           ((or (eql ,char :eof)
-                (char= ,char ,delimiter)))
-         (princ ,char ,token)))))
-       
-       
