6971d3d0a23466131d8baba0fc3d1060595ebbb2
alex_e <alexey.e.egorov@gmail.com>
2014-02-05 16:56:50 +0400

    work in progress

--- procedural.lisp
+++ procedural.lisp
@@ -1,3 +1,7 @@
 (in-package :procedural)
 
 "Special package for macros for generating normal and vertex data"
+
+(defmacro hexagonal-grid (w h height radius)
+  "w and h are dimensions of the grid, height and radius - of each individual hexagon"
+  
